9708ddab4eb302c6b6d7a2e3dd11e316c6bd458f
Jake <69274752+JakeJewsbury@users.noreply.github.com>
2025-02-16 20:56:20 -0500

    Concatonation practice

---
+++ Madlibs copy.py
@@ -0,0 +1,7 @@
+name = input("Please enter a type of tool:")
+town = input("Please enter a fruit: ")
+noun = input("Please enter a noun: ")
+cats = input("Please enter a number: ")
+number = input("Please enter a number from 1-20: ")
+
+print("Your name is {}, you live in {}ville, and you work as a {} repairman. You have {} cats and eat {}000 calories every day".format(name,town,noun,cats,number))
